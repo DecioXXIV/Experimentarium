0086e5a4884a4da53dae7dd1fed8e3953d50ccd3
Riccardo De Cesaris <ric.dc@hotmail.it>
2024-03-10 01:16:15 +0100

    Update vectorize.py

--- 1 [NLP] - Sentence Embedding with BERT/vectorize.py
+++ 1 [NLP] - Sentence Embedding with BERT/vectorize.py
@@ -62,18 +62,31 @@ def bert_extract_features(t, tokenizer, model, device, max_tokens=512):
         t = t[:max_tokens]
 
     tokens = tokenizer.tokenize(t)
+
+    # Special tokens
+        # [CLS]: in a nutshell, represents the whole sentence
+        # [SEP]: indicates the end of a sentence
+        # [PAD]: padding token -> used to fill the rest of a sequence of tokens if it's shorter than the max length (BERT wants sequences all of the same length)
     tokens = ['[CLS]'] + tokens + ['[SEP]']
     tokens = tokens + ['[PAD]'] * ( - len(tokens))
 
+    # Each token is uniquely identified by its ID -> consequently, each ID will be mapped to a unique vector of shape (768,)
     token_ids = tokenizer.convert_tokens_to_ids(tokens)
     token_ids = torch.tensor(token_ids).unsqueeze(0).to(device)
 
+    # Attention Mask: list of binary values. 
+        # If "attention_mask[i] == 1", we are asking BERT to pay attention to the i-th token
+        # Otherwise, if "attention_mask[i] == 0", BERT will ignore the i-th token
     attention_mask = [1 if i != ['[PAD]'] else 0 for i in tokens]
     attention_mask = torch.tensor(attention_mask).unsqueeze(0).to(device)
 
     with torch.no_grad():
         output = model(token_ids, attention_mask)
     vec = output[1].cpu().numpy().flatten()
+
+    # output charactheristics:
+        # output[0] = last_hidden_state -> tensor of shape (batch_size, relevant_tokens, latent_space_dimension): contains the vectors of the last hidden layer of the model for each token in the input sentence
+        # output[1] = pooler_output -> tensor of shape (batch_size, latent_space_dimension): contains the vector representation of the whole sentence (conventionally, it's the vector representation of the [CLS] token)
     
     if device == 'cuda':
         del token_ids
